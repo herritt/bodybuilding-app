e9920566e249337aaf5680545a922abf8c3581c8
Randy Herritt <randy.herritt@rwdi.com>
2024-02-21 09:46:50 -0400

    Created a function to determine the plates needed to meet a specific weight

---
+++ backend/utils/Calculator.js
@@ -0,0 +1,14 @@
+function calculatePlate(targetWeight) {
+	const plates = [45, 35, 25, 15, 10, 5, 2.5];
+	let target = targetWeight - 45;
+	let result = [];
+	for (let i = 0; i < plates.length; i++) {
+		if (target >= plates[i] * 2) {
+			result.push(plates[i]);
+			target -= plates[i] * 2;
+		}
+	}
+	return result;
+}
+
+module.exports = { calculatePlate };
