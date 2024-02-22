29ba69fda5a16daae1516d985ecd7aa40ac08ab0
Randy Herritt <randy.herritt@rwdi.com>
2024-02-22 09:51:19 -0400

    We now have a message to the user letting them know when that they need to have at least 45lbs, what weight is loaded, and when they are at max weight. We handle rounding by telling them what weight is loaded.

--- backend/utils/Calculator.js
+++ backend/utils/Calculator.js
@@ -1,5 +1,5 @@
 function calculatePlate(targetWeight) {
-	const plates = [45, 45, 35, 35, 25, 15, 10, 5, 2.5];
+	const plates = [45, 35, 25, 15, 10, 5, 2.5, 1, 0.75, 0.5, 0.25];
 	let target = targetWeight - 45;
 	let result = [];
 	for (let i = 0; i < plates.length; i++) {
--- frontend/src/components/PlateCalculator/PlateCalculator.js
+++ frontend/src/components/PlateCalculator/PlateCalculator.js
@@ -2,15 +2,20 @@ import "./PlateCalculator.css";
 import React, { useState } from "react";
 
 function PlateCalculator() {
+	// temporary place to store constants until we have the ability to create own weights
+	const allPlates = [45, 35, 25, 15, 10, 5, 2.5, 1, 0.75, 0.5, 0.25];
+
+	const maxWeight = allPlates.reduce((total, plate) => total + plate, 0) * 2 + 45;
+
 	const [weight, setWeight] = useState("");
 	const [plates, setPlates] = useState([]);
+	const [message, setMessage] = useState("Enter the weight you want to lift");
 
 	const handleCalculatePlates = () => {
-		console.log("Calculating plates for weight:", weight);
 		if (!weight) {
-			console.log("Please enter a weight");
-		} else if (parseInt(weight) < 45) {
-			console.log("Weight is less than 45lbs");
+			setMessage("Enter the weight you want to lift");
+		} else if (parseInt(weight) <= 45) {
+			setMessage("Weight has to be greater than weight of the bar (45lb)");
 		} else {
 			// Send the weight to the backend API route for calculating the plates
 			// Replace `apiRoute` with the actual API route
@@ -23,8 +28,13 @@ function PlateCalculator() {
 			})
 				.then((response) => response.json())
 				.then((data) => {
-					// Handle the response from the backend
-					console.log(data);
+					let totalWeight = data.plates.reduce((total, plate) => total + plate, 0) * 2 + 45;
+
+					if (totalWeight >= maxWeight) {
+						setMessage(`Max weight of ${totalWeight}lb loaded!`);
+					} else {
+						setMessage(`${totalWeight}lb loaded!`);
+					}
 					setPlates(data.plates);
 				})
 				.catch((error) => {
@@ -33,16 +43,27 @@ function PlateCalculator() {
 		}
 	};
 
+	const handleKeyPress = (e) => {
+		if (e.key === "Enter") {
+			handleCalculatePlates();
+		}
+	};
+
 	return (
 		<div className="plate-calculator">
 			<div className="plate-calculator-user-input">
-				<input type="text" value={weight} onChange={(e) => setWeight(e.target.value)} />
+				<input
+					type="text"
+					value={weight}
+					onChange={(e) => setWeight(e.target.value)}
+					onKeyDown={handleKeyPress}
+				/>
 				<button onClick={handleCalculatePlates}>Calculate Plates</button>
+				<p className="plate-calculator-user-input-label">{message}</p>
 			</div>
 			<div className="plates-container">
 				{plates.map((plate, index) => {
 					let className = "";
-
 					if (plate >= 35) className = "large-plate";
 					else if (plate >= 10) className = "medium-plate";
 					else className = "small-plate";
